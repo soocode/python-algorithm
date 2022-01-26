2c16ad1b4f92a9daf905d16d5906c95e45e79e5c
Soo Yeon Ji <sooyeonji@Sooui-MacBookPro.local>
2022-01-26 16:42:25 +0900

    Python

---
+++ Greedy/BOJ 1931.py
@@ -0,0 +1,40 @@
+N = int(input())
+
+schedule = []
+count = 1
+
+for i in range(N) :
+    start, end = tuple(map(int, input().split()))
+    schedule.append((start,end))
+    
+schedule.sort(key=lambda x:(x[1],x[0]))
+
+endTime = schedule[0][1]
+
+for i in range(1,N) :
+    
+    if schedule[i][0] >= endTime :
+        count += 1
+        endTime = schedule[i][1]
+
+print(count)
+       
+
+
+
+
+
+        
+        
+
+    
+            
+        
+        
+    
+
+    
+    
+    
+
+    
